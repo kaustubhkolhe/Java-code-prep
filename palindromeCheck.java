2470ed5a95293d8189e70b71ba6ea8a93af5c4de
kaustubhkolhe <70327470+kaustubhkolhe@users.noreply.github.com>
2022-09-22 18:47:30 +0530

    Add files via upload

---
+++ palindromeCheck.java
@@ -0,0 +1,30 @@
+//Program to check if the given string is palindrome or not.
+//This program returns true or false
+public class palindromeCheck {
+    static boolean isPalindrome(String str){
+
+        int start = 0;
+        int end = str.length() - 1;
+
+
+        str = str.toLowerCase();
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end))
+                return false;
+
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+
+
+        String str = "froggyGgOrF";
+
+        System.out.println(isPalindrome(str));
+
+    }
+}
